5b9fb7b4ccde626d8532805c4be18c0d9b91136b
Siva-DL <sivaprasad.digitallync@gmail.com>
2022-07-18 21:22:54 +0530

    singlePageApplication: routes, and HomePage

--- works/src/app/app.module.ts
+++ works/src/app/app.module.ts
@@ -2,15 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { AboutComponent } from './routes/about/about.component';
+import { PortfolioComponent } from './routes/portfolio/portfolio.component';
+import { ContactComponent } from './routes/contact/contact.component';
+import { NotfoundComponent } from './routes/notfound/notfound.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    NotfoundComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '**', component: NotfoundComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
